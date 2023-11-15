54fc65b4a67e990176fa3c08512e4bdd23b71b82
pxvasile <p_vasilevss@abv.bg>
2023-11-16 01:06:16 +0200

    Create Form html and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, useNavigate } from 'react-router-dom';
 
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer";
+import Header from './components/Header/Header';
+import Footer from './components/Footer';
 import Home from './components/Home/Home';
 import Catalog from './components/Catalog/Catalog';
 import AddProduct from './components/AddProduct/AddProduct';
--- src/components/AddProduct/AddProduct.jsx
+++ src/components/AddProduct/AddProduct.jsx
@@ -1,7 +1,32 @@
+import { Link } from "react-router-dom";
+import React from "react";
+import './AddProduct.css';
 
 export default function AddProduct() {
     return (
         <>
+            <div id="add" className="form">
+
+                <h2>ADD Product</h2>
+
+                <label htmlFor="productName">Product Name:</label>
+                <input type="text" id="productName" name="productName" placeholder="Enter Product name here..." /><br />
+
+                <label htmlFor="category">Category:</label>
+                <input type="text" id="category" name="category" placeholder="Enter Category Here..." /><br />
+
+                <label htmlFor="price">Price:</label>
+                <input type="number" id="price" name="price" placeholder="Enter Price Here..." /><br />
+
+                <label htmlFor="material">Material:</label>
+                <select id="material">
+                    <option value="mdf">MDF</option>
+                    <option value="pdch">PDCH</option>
+                    <option value="solid-wood">Solid Wood</option>
+                </select>
+                <button className="btnn"><Link to="#">ADD Product</Link></button>
+
+            </div>
         </>
     )
 }

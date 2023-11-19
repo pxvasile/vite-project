5da00f2c9348a4c93a1cfed7d1fb7fc656946b74
pxvasile <p_vasilevss@abv.bg>
2023-11-19 16:44:20 +0200

    Create Login i Register form

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,7 +1,28 @@
+import { Link } from 'react-router-dom';
+import './Login.css';
+
 export default function Login() {
     return (
         <>
-            <p>Login</p>
+            <div className="form-login">
+                <h2>Login Here</h2>
+                <input type="email" name="email" placeholder="Enter Email Here..." />
+                <input type="password" name="" placeholder="Enter Password Here..." />
+                <button type="submit" className="form-login-btnn" value="Login">Login</button>
+
+                <p className="link">Don't have an account
+                    <Link href="#">Sign up here</Link></p>
+                <p className="liw">Log in with</p>
+
+                <div className="icons">
+                    <a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
+                    <a href="#"><ion-icon name="logo-instagram"></ion-icon></a>
+                    <a href="#"><ion-icon name="logo-twitter"></ion-icon></a>
+                    <a href="#"><ion-icon name="logo-google"></ion-icon></a>
+                    <a href="#"><ion-icon name="logo-skype"></ion-icon></a>
+                </div>
+
+            </div>
         </>
     )
 }
--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,7 +1,30 @@
+import { Link } from 'react-router-dom';
+import './Register.css';
+
 export default function Register() {
     return (
         <>
-            <p>Register</p>
+            <div className="form-register">
+                <h2>Register Here</h2>
+                <input type="text" name="userName" placeholder="Enter Name Here..." />
+                <input type="email" name="email" placeholder="Enter Email Here..." />
+                <input type="password" name="password" placeholder="Enter Password Here..." />
+                <input type="password" name="repass" placeholder="Confirm Password Here..." />
+                <button type="submit" className="form-register-btnn" value="Register">Register</button>
+
+                <p className="link">Have an account
+                    <Link href="#">Login in here</Link></p>
+                <p className="liw">Log in with</p>
+
+                <div className="icons">
+                    <a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
+                    <a href="#"><ion-icon name="logo-instagram"></ion-icon></a>
+                    <a href="#"><ion-icon name="logo-twitter"></ion-icon></a>
+                    <a href="#"><ion-icon name="logo-google"></ion-icon></a>
+                    <a href="#"><ion-icon name="logo-skype"></ion-icon></a>
+                </div>
+
+            </div>
         </>
     )
 }

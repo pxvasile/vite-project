bfe3d84fa2a4daff245b72b165f2c0c7f5ec8d37
pxvasile <p_vasilevss@abv.bg>
2023-12-02 17:08:39 +0200

    Changes after bug

--- client/src/components/Catalog/CatalogItem/Details/ProductDetailsInfo.jsx
+++ client/src/components/Catalog/CatalogItem/Details/ProductDetailsInfo.jsx
@@ -1,93 +1,88 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer, useContext } from 'react';
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 import * as productService from '../../../../services/productService';
 import * as commentService from '../../../../services/commentService';
 
-import './ProductDetailsInfo.css';
+import AuthContext from '../../../../contexts/authContext';
 import AddComment from "./AddComment/AddComment";
+import reducer from './AddComment/commentReducer';
+import './ProductDetailsInfo.css';
 
 export default function ProductDetailsInfo() {
+    const { username, userId } = useContext(AuthContext);
     const { productId } = useParams();
-    const [comments, setComments] = useState([]);
+    // const [comments, setComments] = useState([]);
+    const [comments, dispatch] = useReducer(reducer, []);
     const [productDetails, setProductDetails] = useState({});
 
     useEffect(() => {
         productService.getOne(productId)
             .then(setProductDetails);
-
         commentService.getAll(productId)
-            .then(setComments);
-
+            .then((result) => {
+                dispatch({
+                    type: 'GET_ALL_COMMENTS',
+                    payload: result,
+                });
+            });
     }, [productId]);
 
-    const onSubmit = async (e, values, initialValues) => {
-        e.preventDefault();
-
+    const addCommentHandler = async (values) => {
         values.productId = productDetails._id;
-        
-        await commentService.create(values);
+        values.username = username;
+        const newComment = await commentService.create(values);
+        // newComment.username = username;
+        // setComments(state => ([...state, values]));
+        dispatch({
+            type: 'ADD_COMMENT',
+            payload: newComment,
+        });
+    };
 
-        setComments(state => ([...state, values]));
-        
-        
-    }
-    
-    // const addCommentHandler = async ({ productId, username, comment }) => {
-    //     const createdComment = await commentService.create( //try catch
-    //         productId,
-    //         username,
-    //         comment
-    //     );
-
-    //     setComments(state => [...state, createdComment]);
-    // }
     return (
         <>
             <section id="product-details">
                 <h1>Product Details</h1>
                 <div className="info-section">
-
                     <div className="product-header">
                         <img className="product-img" src={productDetails.imageUrl} />
                         <h1>{productDetails.productName}</h1>
                         <span className="material">Material</span>
                         <p className="type">{productDetails.material}</p>
                     </div>
-
                     <p className="text">
                         Set in a world where fantasy creatures live side by side with humans. A human cop is forced to work
                         with an Orc to find a weapon everyone is prepared to kill for. Set in a world where fantasy
                         creatures live side by side with humans. A human cop is forced
                         to work with an Orc to find a weapon everyone is prepared to kill for.
                     </p>
-
                     {/* Edit/Delete buttons ( Only for creator of this game ) */}
                     <div className="buttons">
                         <Link href="#" className="button">Edit</Link>
                         <Link href="#" className="button">Delete</Link>
                     </div>
+                    {userId === productDetails._ownerId && (<div className="buttons">
+                        <Link to={Path.ProductDetailsEdit} className="button">Edit</Link>
+                        <Link to={Path.Delete} className="button">Delete</Link>
+                    </div>)}
                 </div>
-
                 <div className="details-comments">
                     <h2>Comments:</h2>
                     <ul>
-                        {comments.map((comment) => (
-                            <li key={comment._id} className="comment">
-                                <p>{comment.username}: {comment.comment}</p>
+                        {comments.map(({ _id, username, comment }) => (
+                            <li key={_id} className="comment">
+                                <p>{username}: {comment}</p>
                             </li>
                         ))}
                     </ul>
-
                     {comments.length === 0 && (
                         <p className="no-comment">No comments yet!</p>
                     )}
                 </div>
-
             </section>
-
-            <AddComment onSubmit={onSubmit} />
+            <AddComment addCommentHandler={addCommentHandler} />
         </>
     )
 }

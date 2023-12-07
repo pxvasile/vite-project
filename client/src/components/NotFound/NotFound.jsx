8916a548278e23476979d572113620203f84fc65
pxvasile <p_vasilevss@abv.bg>
2023-12-07 21:34:39 +0200

    Not Found Page

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -1,5 +1,13 @@
+import './NotFound.css'
+
 export default function NotFound() {
-    return(
-        <p>404</p>
+    return (
+        <>  
+            <div className="not-found">        
+                <h1>404</h1>
+                <h2>Not Found</h2>
+                <h3>The resource requested could not be found on this server!</h3>
+            </div>
+        </>
     )
 }
